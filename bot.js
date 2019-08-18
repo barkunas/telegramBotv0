f34614222f6b8180334836a75dfaefbf2a548f40
barkuns <45297029+barkuns@users.noreply.github.com>
2019-08-19 00:46:53 +0300

    resolved issue with Promise
    resolved issue with Promise and updated images

--- bot.js
+++ bot.js
@@ -6,31 +6,32 @@ var userData = {}
 
 var token = '976224147:AAGkrKkqAwWVHZUgFp_O7WePkJVnj08lbEA';
 
-var myBot = new TelegramBot(token, {polling: true});
+var myBot = new TelegramBot(token, { polling: true });
 
 
 
 var router = require('./router')
 
 myBot.onText(/\/echo (.+)/, (msg, match) => {
-  
-    const chatId = msg.chat.id;
-    const resp = match[1]; 
-    console.log(JSON.stringify(msg))
-    var testText
-    myBot.sendMessage(chatId, resp);
-  });
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  console.log(JSON.stringify(msg))
+  var testText
+  myBot.sendMessage(chatId, resp);
+});
 
 
 myBot.on('message', function (msg) {
-    var selfMybot = this
-    var chatId = msg.chat.id;
-    var text = msg.text
-    try{
+  var selfMybot = this
+  var chatId = msg.chat.id;
+  var text = msg.text
+  try {
     //console.log(router.messageRouter)
-    router.messageRouter(text,msg,selfMybot)} catch(e){console.log(e)}
-    console.log(JSON.stringify('сообщение от '+chatId+"||текст - "+text+"||"))
-    
+    router.messageRouter(text, msg, selfMybot)
+  } catch (e) { console.log(e) }
+  console.log(JSON.stringify('сообщение от ' + chatId + "||текст - " + text + "||"))
+
 });
 
 
--- router.js
+++ router.js
@@ -6,68 +6,80 @@ const fs = require('fs');
 console.log('собираем модули...')
 var userStart = require('./router/userStart')
 console.log('...модули собраны')
-module.exports.messageRouter = function messageRouter (message,data,selfMybot){
-    switch(message){
+module.exports.messageRouter = function messageRouter(message, data, selfMybot) {
+    switch (message) {
         case '/start':
             console.log('сообщение определено как /start');
-            start(message,data,selfMybot)
+            start(message, data, selfMybot)
             break;
         case '/pokormit':
-            pokormit(message,data,selfMybot)
+            pokormit(message, data, selfMybot)
             break;
     }
 }
-function pokormit(message,data,selfMybot){
-    var dataPath = __dirname+"\\data.json";
+function pokormit(message, data, selfMybot) {
+    var botId = '976224147'
+    var dataPath = __dirname + "\\data.json";
     var usersData;
     usersData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
     console.log(usersData);
-    var userId = ""+data.from.id;
-    var name = "test"+userId+"_by_kakakaFriendBot";
-    var title = ""+name;
+    var userId = "" + data.from.id;
+    var stickerName = "test9" + userId + "_by_kakakaFriendBot";
+    var title = "" + stickerName;
+    var stickerId = usersData[userId]['stickerId']
     //console.log(usersData[userId][rank])
-    if(+usersData[userId]['rank']<99){
-        var rank = usersData[userId]['rank']+1;
-        console.log('уровень пользователя'+rank)
-        var img = fs.createReadStream(__dirname+'\\images'+'\\'+rank+'.png');
-        var emojis = "👍";
-        selfMybot.sendSticker(data.chat.id,img)
-        usersData[userId]['rank'] = rank;
-        json = JSON.stringify(usersData);
-        fs.writeFile(dataPath, json, 'utf8',function(err) {
-            if (err) throw err;
-            console.log('complete');
-            });
+    if (+usersData[userId]['rank'] < 99) {
+        var rank = usersData[userId]['rank'] + 1;
+        console.log('уровень пользователя' + rank);
+        var img = fs.createReadStream(__dirname + '\\images' + '\\' + rank + '.png');
+        selfMybot.addStickerToSet(botId, stickerName, img, "😂").then(() => {
+            selfMybot.getStickerSet(stickerName).then((stickerObj) => {
+                var stickerId = stickerObj.stickers[stickerObj.stickers.length-1].file_id
+                selfMybot.sendSticker(data.chat.id, stickerId)
+                usersData[userId] = { 'rank': rank, 'stickerId': stickerId }
+                json = JSON.stringify(usersData);
+                fs.writeFile(dataPath, json, 'utf8', function (err) {
+                    if (err) throw err;
+                    console.log('complete');
+                })
+            },(err)=>(console.log(err)))
+        }, (err) => console.log(err));
     }
 }
 
-function start(message,data,selfMybot){
-    var dataPath = __dirname+"\\data.json"
+function start(message, data, selfMybot) {
+    var dataPath = __dirname + "\\data.json"
     var usersData;
 
     usersData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
     console.log(usersData)
-    var userId = ""+data.from.id;
-    var name = "test"+userId+"_by_kakakaFriendBot";
-    var title = ""+name;
-    if(!usersData[userId]){
-        
-    console.log(__dirname)
-    var img = fs.createReadStream(__dirname+'\\images'+'\\0.png')
-    var emojis = "👍"
-    //console.log(img)
-    console.log("t.me/addstickers/"+name)
-    try{
-    selfMybot.createNewStickerSet(userId, name, title,img,emojis)} catch (e) {console.log("ошибка создания стикера || "+e)};
-    selfMybot.sendSticker(data.chat.id,img)
-    usersData[userId] = {'rank' : 0}
-    json = JSON.stringify(usersData); //convert it back to json
-    fs.writeFile(dataPath, json, 'utf8',function(err) {
-        if (err) throw err;
-        console.log('complete');
-        });
+    var userId = "" + data.from.id;
+    var stickerName = "test9" + userId + "_by_kakakaFriendBot";
+    var stickerNameTemp = stickerName
+    var title = "" + stickerName;
+    if (!usersData[userId]) {
 
-    } else {console.log('этот пользователь уже стартанул')}
+        console.log(__dirname)
+        var img = fs.createReadStream(__dirname + '\\images' + '\\0.png')
+        var emojis = "😂"
+        console.log(img + 'PNGSTIKERHERE-------------------------------------')
+        console.log("t.me/addstickers/" + stickerName)
+        selfMybot.createNewStickerSet(userId, stickerName, title, img, emojis).then((val) => {
+            //console.log(val)
+            selfMybot.getStickerSet(stickerNameTemp).then((stickerObj) => {
+                console.log(stickerObj)
+                var stickerId = stickerObj.stickers[0].file_id
+                selfMybot.sendSticker(data.chat.id, stickerId)
+                usersData[userId] = { 'rank': 0, 'stickerId': stickerId }
+                json = JSON.stringify(usersData); //convert it back to json
+                fs.writeFile(dataPath, json, 'utf8', function (err) {
+                    if (err) throw err;
+                    console.log('complete');
+                })
+            })
+        }, (err) => console.log(err));
+
+    } else { console.log('этот пользователь уже стартанул') }
     //console.log(usersData)
 
 }
